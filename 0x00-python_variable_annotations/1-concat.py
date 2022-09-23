1756888095b75bba814c680835c0e0e80886becc
Sebastian Calle <jusemonca@gmail.com>
2022-09-23 11:03:06 -0500

    Function to concat two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Module with function concat
+"""
+
+
+def concat(str1: str, str2:str) -> str:
+    """
+        Args:
+            str1: The first parameter
+            str2: The second parameter
+        Return:
+            concat two string
+    """
+
+    return str1 + str2
