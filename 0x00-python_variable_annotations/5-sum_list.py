88d31258dd0013a1ca65a00f4e997d33394f2cc8
Sebastian Calle <jusemonca@gmail.com>
2022-09-23 11:17:44 -0500

    Function sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python3
+"""
+    Module with function sum_list
+"""
+
+
+def sum_list(input_list:list[float]) -> float:
+    """
+        Args:
+            input_list: The first parameter
+        Return:
+            sum elements of the list
+    """
+
+    return sum(input_list)
