a7f28e58ba935eff410296478c11a1b3e9a619f6
Sebastian Calle <jusemonca@gmail.com>
2022-09-23 11:51:14 -0500

    Create basic cache

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Module for BasicCache
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
+
+
+
