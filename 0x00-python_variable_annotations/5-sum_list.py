f8824917060eb3415f22f55d2887b11908e6a1ab
Sebastian Calle <jusemonca@gmail.com>
2022-09-23 11:21:44 -0500

    Import List typing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,14 +2,14 @@
 """
     Module with function sum_list
 """
+from typing import List
 
 
-def sum_list(input_list:list[float]) -> float:
+def sum_list(input_list:List[float]) -> float:
     """
         Args:
             input_list: The first parameter
         Return:
             sum elements of the list
     """
-
     return sum(input_list)
