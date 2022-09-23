283772faab3dcef3d2d563c719f1f2b743d059e5
Sebastian Calle <jusemonca@gmail.com>
2022-09-23 11:09:04 -0500

    Function to floor a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python3
+"""
+    Module with function concat
+"""
+
+
+def floor(n: float) -> int:
+    """
+        Args:
+            n: The first parameter
+        Return:
+            floor of a number
+    """
+
+    return int(n)
+
