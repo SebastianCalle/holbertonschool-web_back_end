2f5139aa554c04e165818855e50ffea8d699f974
Sebastian Calle <jusemonca@gmail.com>
2022-09-24 12:46:13 -0500

    Function to cast float to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Module with function to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Args:
+            n: The first parameter
+        Return:
+            str of a number
+    """
+
+    return str(n)
