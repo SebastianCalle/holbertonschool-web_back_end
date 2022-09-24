b8b3d95e2d9a0f19498c65b7dc108c05db56ef69
Sebastian Calle <jusemonca@gmail.com>
2022-09-23 23:02:04 -0500

    Create LRUCaching

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,41 @@
+#!/user/bin/python3
+"""
+    Module for LRUCache
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ LRUCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        """ Initialize
+        """
+        self.lru_order = OrderedDict()
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.lru_order[key] = item
+            self.lru_order.move_to_end(key)
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            remove = next(iter(self.lru_order))
+            del self.cache_data[remove]
+            print(f"DISCARD: {remove}")
+
+        if len(self.lru_order) > BaseCaching.MAX_ITEMS:
+            self.lru_order.popitem(last=False)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            self.lru_order.move_to_end(key)
+        return self.cache_data.get(key)
