10fb94ca5bb835b6c563a742f9e90e696da927ef
Sebastian Calle <jusemonca@gmail.com>
2022-09-26 15:51:02 -0500

    Add docstring

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,13 @@ from time import sleep
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    Args:
+        max_delay: First argument
+
+    Returns:
+        float sleep
+    """
     delay = random.uniform(0, max_delay)
     sleep(delay)
     return delay
