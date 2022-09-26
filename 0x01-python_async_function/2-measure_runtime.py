8bda1608267b05016119bff51bf3dcb1adf31bfe
Sebastian Calle <jusemonca@gmail.com>
2022-09-26 16:41:18 -0500

    Upgrade function measure_time

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+    Module with function measure_time
+"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Args:
+        n: times
+        max_delay:  max delay
+
+    Returns:
+        float
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - start
+    return total_time / n
+
+
+
+
